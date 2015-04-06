bb1d83268c00e60817bd7813d748eeaaf1158650
Keith Williams <kawillia@vt.edu>
2015-04-06 19:07:03 -0400

    Working version

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,113 @@
 
+var output = document.getElementById('output');
+var canvas = document.getElementById('equalizerCanvas');
+var canvasContext = canvas.getContext('2d');
+
+var player = document.createElement('audio');
+var audio = document.createElement('audio');
+var audioContext = new AudioContext();
+var analyser = audioContext.createAnalyser();
+var animationFrame = null;
+var isPaused = false;
+
+document.body.appendChild(player);
+source = audioContext.createMediaElementSource(audio);
+source.connect(analyser);
+
+function stopEvent (event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function windowResize () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.onresize = windowResize;
+windowResize();
+
+function reduce (array, size) {
+  if (size >= array.length) { return array; }
+  var newArray = [],
+      step = parseInt(array.length / size);
+  
+  for (var i = 0; i < array.length; i += step) {
+    var sum = 0;
+    for (var j = 0; j < step && (i + j) < array.length; j++) {
+      sum += array[i + j];
+    }
+    newArray.push(parseInt(sum / step));
+  }
+  
+  return newArray;
+}
+
+function renderFrame (audio, analyser) {
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(frequencyData);
+  frequencyData = reduce(frequencyData, canvas.width / 10);
+  
+  var colWidth = canvas.width / frequencyData.length;
+  var colHeight = canvas.height / 255;
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'rgba(0, 0, 0, 0.3)';
+  canvasContext.strokeStyle = 'rgba(88, 197, 63, 0.6)';
+  
+  canvasContext.beginPath();
+  canvasContext.moveTo(0, canvas.height);
+  for (var i = 1; i < frequencyData.length; i++) {
+    canvasContext.lineTo(i * colWidth, canvas.height - 10 - frequencyData[i] * colHeight);
+  }
+  canvasContext.lineTo(canvas.width, canvas.height);
+  canvasContext.closePath();
+  canvasContext.fill();
+  canvasContext.stroke();
+  
+  animationFrame = requestAnimationFrame(function () {
+    renderFrame(audio, analyser);
+  });
+}
+
+function playAudio (url, name) {
+  audio.autoplay = true;
+  player.autoplay = true;
+  audio.src = url;
+  player.src = url;
+  audio.play();
+  player.play();
+  $('h1').html(name);
+  cancelAnimationFrame(animationFrame);
+  renderFrame(audio, analyser);
+}
+
+function continueAudio() {
+  audio.play();
+  player.play();
+  isPaused = false;
+}
+
+function pauseAudio() {
+  audio.pause();
+  player.pause();
+  isPaused = true;
+}
+
+function dropAudio (event) {
+  stopEvent(event);
+  var file = event.originalEvent.dataTransfer.files[0],
+      url = URL.createObjectURL(file);
+      playAudio(url, file.name);
+}
+
+$(window)
+  .on('dragover', stopEvent)
+  .on('dragenter', stopEvent)
+  .on('drop', dropAudio);
+
+$('#pauseTunes').on('click', function() {
+  if (isPaused)
+    continueAudio();
+  else
+    pauseAudio();
+});
